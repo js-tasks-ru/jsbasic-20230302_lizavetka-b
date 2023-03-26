4deecef4641c144895c70d3b67f15b990c637508
Liza Brouk <lizavetka.b@gmail.com>
2023-03-26 22:04:04 +0300

    trying to do module4

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,25 @@
 function highlight(table) {
-  // ваш код...
+  for (let row of table.rows) {
+   
+    if (!row.cells[3].dataset.available) {
+      row.setAttribute('class', 'hidden');
+    } else if (row.cells[3].dataset.available == 'true') {
+      row.setAttribute('class','available');
+    } else {
+      row.setAttribute('class', 'unavailable');
+    }
+    
+    if (row.cells[2].textContent == 'm') {
+      row.classList.add('male');
+    } else if (row.cells[2].textContent == 'f'){
+      row.classList.add('female');
+     
+    }
+    
+    if (row.cells[1].innerHTML < 18) {
+      row.style = "text-decoration: line-through";
+    }
+    
+  }
+  return (table);
 }
