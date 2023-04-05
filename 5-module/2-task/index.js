6c3e6905761028803e1977a63c490d7bd1dc9d31
Liza Brouk <lizavetka.b@gmail.com>
2023-04-05 14:21:34 +0300

    module5 fixed, module 6 done

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -2,6 +2,7 @@ function toggleText() {
   let button = document.querySelector('.toggle-text-button');
   button.addEventListener('click', toggle);
   function toggle() {
-    text.hidden == true ? text.hidden = false : text.hidden = true;
+    //text.hidden == true ? text.hidden = false : text.hidden = true;
+    text.hidden = !text.hidden;
   }
 }
--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -12,7 +12,55 @@
  *      }
  *
  */
+
 export default class UserTable {
+  elem = null;
+  #rows = [];
+  
   constructor(rows) {
+    this.#rows = rows;
+    this.elem = this.#render();
   }
+
+  #tableTemplate() {
+    let table = document.createElement('table');
+    let header = document.createElement('thead');
+    header.innerHTML = `<tr>
+        <th>Имя</th>
+        <th>Возраст</th>
+        <th>Зарплата</th>
+        <th>Город</th>
+        <th></th>
+        </tr>`;
+    table.append(header);
+    let body = document.createElement('tbody');
+
+    for (let user of this.#rows) {
+     let row = document.createElement('tr');
+      row.insertCell(0).textContent = user.name;
+      row.insertCell(1).textContent = user.age;
+      row.insertCell(2).textContent = user.salary;
+      row.insertCell(3).textContent = user.city;
+      row.insertCell(4).innerHTML = '<button>X</button>';
+      body.append(row);
+    }
+    
+    table.append(body);
+    return table;
+  } 
+
+  #closeOnClick = (event) => {
+    if (event.target.tagName != 'BUTTON') return;
+    let clicked = event.target.parentNode.parentNode.rowIndex;
+    this.elem.deleteRow(clicked);
+
+  }
+
+  #render() {
+    let elem = this.#tableTemplate();
+    elem.addEventListener('click', this.#closeOnClick);
+    return elem;
+  }
+
+
 }
